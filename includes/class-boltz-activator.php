87294ca46a2582a964ae819262a2ee6d3cfad90a
Norboo <istrate.paul@gmail.com>
2018-05-04 11:29:30 +0800

    First commit

---
+++ includes/class-boltz-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.eruisw.com
+ * @since      1.0.0
+ *
+ * @package    Boltz
+ * @subpackage Boltz/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Boltz
+ * @subpackage Boltz/includes
+ * @author     eRui Design <contact@eruisw.com>
+ */
+class Boltz_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-boltz-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.eruisw.com
+ * @since      1.0.0
+ *
+ * @package    Boltz
+ * @subpackage Boltz/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Boltz
+ * @subpackage Boltz/includes
+ * @author     eRui Design <contact@eruisw.com>
+ */
+class Boltz_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
